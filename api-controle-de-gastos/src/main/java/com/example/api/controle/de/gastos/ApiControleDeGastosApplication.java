6e34a8f44e21a18d7abf989109c27b0d444a8f63
Fernando Nassar <fernandonassar14@yahoo.com.br>
2022-04-27 21:11:50 -0300

    arquivo inicial do projeto

---
+++ api-controle-de-gastos/src/main/java/com/example/api/controle/de/gastos/ApiControleDeGastosApplication.java
@@ -0,0 +1,13 @@
+package com.example.api.controle.de.gastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControleDeGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControleDeGastosApplication.class, args);
+	}
+
+}
---
+++ api-controle-de-gastos/src/test/java/com/example/api/controle/de/gastos/ApiControleDeGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api.controle.de.gastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControleDeGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
